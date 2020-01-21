0425e87ff9896beef8d3fe3063392426532d35d8
김환웅 <onplay0810@o365cbe.net>
2020-01-21 15:47:57 +0900

    function_support_space_input

--- GitTest/소스.c
+++ GitTest/소스.c
@@ -9,7 +9,7 @@ int main(void)
 	
 	printf("이름을 입력하세요 (최대 19자까지 입력)\n");
 	printf("======================================\n");
-	scanf("%s", name);
+	scanf("%[^\n]", name);
 
 	printf("\n입력한 이름은 %s 입니다.\n", name);
 	printf("입력한 이름의 글자수는 %d 입니다.\n", countNamesize(name));
@@ -20,12 +20,15 @@ int countNamesize(char* str)
 {
 	int i;
 	int is_name_kor = 0;
+	int count = 0;
 
 	if (str[0] & 0x80) is_name_kor++;
 
 	for (i = 0; i < 39; i++)
 	{
-		if (str[i] == '\0') return (i / (is_name_kor + 1));
+		if (str[i] == '\0') return (count / (is_name_kor + 1));
+		if (str[i] == ' ') continue;
+		count++;
 	}
 	return (i / (is_name_kor + 1));
 }
