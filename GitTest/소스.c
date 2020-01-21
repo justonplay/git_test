a4d8b59089317b2eb3f391e8331f55f46f54d89d
김환웅 <onplay0810@o365cbe.net>
2020-01-21 10:47:23 +0900

    function_count_namesize

--- GitTest/소스.c
+++ GitTest/소스.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int countNamesize(char* str);
+
 int main(void)
 {
 	char name[39];
@@ -10,6 +12,17 @@ int main(void)
 	scanf("%s", name);
 
 	printf("\n입력한 이름은 %s 입니다.\n", name);
-
+	printf("입력한 이름의 글자수는 %d 입니다.\n", countNamesize(name));
 	return 0;
+}
+
+int countNamesize(char* str)
+{
+	int i;
+
+	for (i = 0; i < 39; i++)
+	{
+		if (str[i] == '\0') return i;
+	}
+	return i;
 }
